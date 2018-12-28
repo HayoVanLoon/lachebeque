2201e27a20e9ede0992a94ef6184a652a477c3cc
Hayo van Loon <hayo.vanloon@incentro.com>
2018-12-28 11:26:59 +0100

    update model deploy script

--- tensorflow/create_model.sh
+++ tensorflow/create_model.sh
@@ -5,8 +5,10 @@ REGION=us-east1
 
 TFVERSION=1.12
 
-MODEL=lachebeque
-MODEL_VERSION="0_1"
+MODULE=lachebeque
+
+MODEL="${1}"
+MODEL_VERSION="${2}"
 
 OUTDIR=gs://${PROJECT}/${MODEL}/trained/${MODEL_VERSION}
 
@@ -16,9 +18,9 @@ echo "Deleting and deploying $MODEL $MODEL_VERSION from $MODEL_LOCATION ... this
 #gcloud ml-engine versions delete ${MODEL_VERSION} --model ${MODEL}
 #gcloud ml-engine models delete ${MODEL}
 
-gcloud ml-engine models create ${MODEL} \
+gcloud ml-engine models create ${MODULE} \
     --regions ${REGION}
-gcloud ml-engine versions create ${MODEL_VERSION} \
-    --model ${MODEL} \
+gcloud ml-engine versions create "${MODEL}_${MODEL_VERSION}" \
+    --model ${MODULE} \
     --origin ${MODEL_LOCATION} \
     --runtime-version=${TFVERSION}
